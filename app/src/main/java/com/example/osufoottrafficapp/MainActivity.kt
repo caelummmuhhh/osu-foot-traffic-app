ae82c6f5f3018dd53af6f416459f595899159aae
caelummmuhhh <115360461+caelummmuhhh@users.noreply.github.com>
2025-02-21 18:21:29 -0500

    Create a Google Maps Fragment and display it in MainActivity

--- app/src/main/java/com/example/osufoottrafficapp/MainActivity.kt
+++ app/src/main/java/com/example/osufoottrafficapp/MainActivity.kt
@@ -2,23 +2,55 @@ package com.example.osufoottrafficapp
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.osufoottrafficapp.databinding.ActivityMainBinding
+import com.example.osufoottrafficapp.fragment.MapFragment
+//import com.google.android.gms.maps.GoogleMap
+//import com.google.android.gms.maps.OnMapReadyCallback
+//import com.google.android.gms.maps.SupportMapFragment
+//import com.google.android.gms.maps.model.LatLng
+//import com.google.android.gms.maps.model.MarkerOptions
+
 
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()/*, OnMapReadyCallback*/ {
     private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_fragment_container, MapFragment())
+                .commit()
+        }
+        //setContentView(R.layout.fragment_map)
+        /*
+
+        // Get a handle to the fragment and register the callback.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+        */
     }
+/*
+    // Get a handle to the GoogleMap object and display marker.
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
+
+        Log.d(TAG, "onMapReady called!")
+    }
+*/
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart() called")
