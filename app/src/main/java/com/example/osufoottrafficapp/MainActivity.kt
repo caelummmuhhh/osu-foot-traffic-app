c6abd3d835d7bc99dbb65a2ee6c5f16499e816a3
Toaster2112 <grabowski.61@osu.edu>
2025-02-21 03:58:51 -0500

    Implemented Activity's life cycle methods onCreate, onStart, onResume, onPause, onStop, onDestroy all implemented MainACtivityBinding was enabled - build.gradle.kts had to be modified to allow this

--- app/src/main/java/com/example/osufoottrafficapp/MainActivity.kt
+++ app/src/main/java/com/example/osufoottrafficapp/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.osufoottrafficapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.osufoottrafficapp.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
